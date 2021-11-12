8a0ae69bedfafea77955c3d6521c8a93edcfd6f9
pybun <92768714+pybun@users.noreply.github.com>
2021-11-12 12:38:04 +0100

    Update PA01.py

--- Descartes' rule of signs/PA01.py
+++ Descartes' rule of signs/PA01.py
@@ -1,15 +1,17 @@
-def sign(z):
-    if z >= 0:
-        return 1
-    else:
-        -1
-
 def roots(a, b, c, d, e, f):
-    array = [a, b, c, d, e, f]
+    polynomial = [1, a*d, a*e+b*d, a*f+b*e+c*d, b*f+c*e, c*f]
 
-    c = sum(sign(x) != sign(y) for x, y in zip(array, array[1:]))
+    last_sign = 1
+    sign_changes = 0
 
-    if c % 2 == 0:
-        print('Das Polynom hat eine gerade Anzahl von positiven reellen Wurzeln.') 
+    for x in polynomial:
+        if x != 0:
+            sign = x / abs(x)
+        if sign == -last_sign:
+            sign_changes += 1
+            last_sign = sign
+
+    if sign_changes % 2 == 0:
+        return 'Das Polynom hat eine gerade Anzahl von positiven reellen Wurzeln.'
     else:
-        print('Das Polynom hat eine ungerade Anzahl von positiven reellen Wurzeln.')
+        return 'Das Polynom hat eine ungerade Anzahl von positiven reellen Wurzeln.'
